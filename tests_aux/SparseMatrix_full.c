2899b85e9200c06da3ff306e910328b7f7b518b0
Simon Lee <simonlee98853@gmail.com>
2023-10-21 00:14:05 -0400

    change sparse matrix test cases

--- tests_aux/SparseMatrix_full.c
+++ tests_aux/SparseMatrix_full.c
@@ -3,7 +3,7 @@
 int main() {
     int** M = initializeMatrix(4, 2, (int[]){0,4,0,5,0,0,2,0});
     int** S_act = initializeMatrix(3, 4, (int[]){0,0,0,0,0,0,0,0,0,0,0,0});
-    int D[2] = {2,4};
+    int D[2] = {4,2};
     SparseMatrix(M, S_act, D);
     freeMatrix(4, M);
     freeMatrix(3, S_act);
